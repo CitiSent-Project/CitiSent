aec6eb99608eff9c566362e7bf57b600b476af4b
Marc Darren <marcdarrenguev08@gmail.com>
2026-03-17 19:16:31 +0800

    Implemented the function for notification bell

--- CitiSent-Mobile/modules/home/index.js
+++ CitiSent-Mobile/modules/home/index.js
@@ -5,6 +5,7 @@ export { default as HomeHeader } from "../../components/home/HomeHeader";
 export { default as HomeHero } from "../../components/home/HomeHero";
 export { default as LatestReportCard } from "../../components/home/LatestReportCard";
 export { default as NewsCard } from "../../components/home/NewsCard";
+export { default as NotificationBellButton } from "../../components/home/NotificationBellButton";
 export { default as SectionHeader } from "../../components/home/SectionHeader";
 
 export { reportsApi } from "../../services/reports";
